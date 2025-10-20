166f092a2ac1286d0a6ad100dc6cfba12e70010b
Ali Marami <marami@ironix.com>
2025-10-19 20:08:22 -0400

    session 2 updates

--- apc-example.R
+++ apc-example.R
@@ -1,6 +1,8 @@
 # Install and load the apc package if not already installed
 if (!require(apc)) install.packages("apc")
 library(apc)
+# The data set has counts of male deaths by age 25–89 and by 1967–2007. There is no direct
+# measure for the exposure to asbestos.
 data.asbestos <- data.asbestos()
 data.asbestos
 apc.fit.table(data.asbestos, "poisson.response")
